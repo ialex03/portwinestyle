4ec56bbc1f01ede15a41047b5a2020cb72162adb
Alexandra Ipatova <alexandraipatova@Alexandras-MacBook-Pro.local>
2021-04-27 17:18:29 +0100

    feat:adding-dropdown-menu-in-stats-and-history

--- admin/includes/sidebar.inc.php
+++ admin/includes/sidebar.inc.php
@@ -11,15 +11,18 @@
         <!-- Sidebar Navidation Menus--><span class="heading">Principal</span>
         <ul class="list-unstyled">
           <li class="active"><a href="dashboard.php"> <i class="icon-home"></i>Home </a></li>
-          <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Estatísticas</a></li>
-          <!--
-          <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Estatísticas</a>
-            <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-              <li><a href="#">Page</a></li>
-              <li><a href="#">Page</a></li>
-              <li><a href="#">Page</a></li>
+          <li><a href="#statsdropdown" aria-expanded="false" data-toggle="collapse"> <i class="fa fa-bar-chart"></i>Estatísticas</a>
+            <ul id="statsdropdown" class="collapse list-unstyled ">
+              <li><a href="#"><i class="fa fa-product-hunt"></i>Produtos</a></li>
+              <li><a href="#"><i class="fa fa-exclamation"></i>Encomendas</a></li>
             </ul>
-          </li>-->
+          </li>
+          <li><a href="#historydropdown" aria-expanded="false" data-toggle="collapse"> <i class="fa fa-history"></i>Histórico</a>
+            <ul id="historydropdown" class="collapse list-unstyled ">
+              <li><a href="#"><i class="fa fa-envelope-o"></i>Todos os emails</a></li>
+              <li><a href="#"><i class="fa fa-envelope-o"></i>Emails Newsletter</a></li>
+            </ul>
+          </li>
         </ul><span class="heading">Tabelas</span>
         <ul class="list-unstyled">
           <li> <a href="<?php echo $arrSETTINGS['url_site_admin'].'/tables.php?table=produtos'?>"> <i class="icon-grid"></i>Tabela Produtos</a></li>
