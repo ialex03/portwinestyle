0be01bb36c9a8b59519756b239d031e42a3e1ed6
Alexandra Ipatova <31103582@colgaia.pt>
2021-04-29 18:26:23 +0100

    typo: eliminar produtos;feat: eliminar banner

--- admin/tables/banner/banner.eliminar.php
+++ admin/tables/banner/banner.eliminar.php
@@ -10,9 +10,9 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 
-    $query="DELETE FROM parceiros WHERE id=".$_GET['id'];
+    $query="DELETE FROM banner WHERE id=".$_GET['id'];
     db_query($query);
-    $query="DELETE FROM produtos_idiomas WHERE id=".$_GET['id']." AND idioma=".$_SESSION['idioma'];
+    $query="DELETE FROM banner_idiomas WHERE id=".$_GET['id']." AND idioma=".$_SESSION['idioma'];
     db_query($query);
     header('Location:/portwinestyle/admin/tables.php?table=produtos&deletesuccess=true');
 
--- admin/tables/banner/banner.retornar.php
+++ admin/tables/banner/banner.retornar.php
@@ -10,6 +10,6 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 
-    $query="UPDATE produtos SET is_active=1 WHERE id=".$_GET['id'];
-    header('Location:/portwinestyle/admin/tables.php?table=produtos&returnsuccess=true');
+    $query="UPDATE banner SET is_active=1 WHERE id=".$_GET['id'];
+    header('Location:/portwinestyle/admin/tables.php?table=banner&returnsuccess=true');
 ?>
